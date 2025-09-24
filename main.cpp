e50c19677c89aa7d01368fc73f876662f228fcde
Valeria Bermudez <2430215@upv.edu.mx>
2025-09-23 23:42:51 -0600

    actualizar stock y producto con mayor precio

--- main.cpp
+++ main.cpp
@@ -13,6 +13,17 @@ int main() {
     int producto;
     //variable del tamaño de arreglos
     int tamano=sizeof(codigo)/sizeof(codigo[0]);
+    //variable contador de elementos del array
+    int contador=5; //5 porque los primeros 5 datos ya estan llenos
+    //variable para el nuevo elemento
+    int codEscogido;
+    int nuevoStock;
+    int encontrado=-1;
+    //para el producto mas caro
+    float mayor;
+    int mayorProducto;
+
+
     
     std::cout<<" "<<std::endl;
     std::cout<<"Sistema de Inventario de 'El Martillo'"<<std::endl;
@@ -65,7 +76,32 @@ int main() {
         break;
 
         case 2:
-            std::cout<<"oli"<<std::endl;
+            std::cout<<"Ingresa el codigo del producto para actualizar"<<std::endl;
+            std::cout<<" "<<std::endl;
+            std::cin>>codEscogido;
+
+
+            for (int i = 0; i < tamano; i++){
+                if (codigo[i] == codEscogido){
+                    encontrado=i;
+                    break;
+                }
+            }
+
+            if (encontrado!=-1){
+                std::cout<<"Producto: "<<nombre[encontrado]<<std::endl;
+
+                std::cout<<"Actualize el stock: "<<std::endl;
+                std::cout<<" "<<std::endl;
+                std::cin>>nuevoStock;
+                
+
+                stock[encontrado]=nuevoStock;
+                std::cout<<"Stock actualizado: "<<stock[encontrado]<<std::endl;
+            } else{
+                std::cout<<"El codigo no existe"<<std::endl;
+            }
+
         break;
         case 3:
             std::cout<<"--REPORTE DE INVENTARIO--"<<std::endl;
@@ -80,6 +116,17 @@ int main() {
             
         break;
         case 4:
+        mayor = precio[0];
+        for (int i = 0; i < tamano; i++){
+            if (precio[i]>mayor){
+                mayor=precio[i];
+                mayorProducto=i;
+            }
+            
+        }
+
+        std::cout<<"El producto con mayor precio es: "<<nombre[mayorProducto]<<std::endl;
+        
         break;
         case 5:
             return 0;
